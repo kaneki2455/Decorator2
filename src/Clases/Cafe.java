19c7fbaf55520449f2707b3abeeb6df34fea2cff
Piero <pier0ocj77@gmail.com>
2026-06-20 22:51:14 -0500

    refactor: encapsular atributos de Cafe con private y agregar tamaño, para acceder a tamaño y a descripcion , lo realizaremos mediante seters y geters

--- src/Clases/Cafe.java
+++ src/Clases/Cafe.java
@@ -1,10 +1,23 @@
 package Clases;
 
 public abstract class Cafe {
-    String descripcion = "Bebida desconocida";
+    private String descripcion = "Bebida desconocida";
+    private String tamaño;
 
     public String getDescripcion() {
         return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
     }
 
     public abstract double costo();
