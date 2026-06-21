d1b3f1b1d1ddfa9ce1532e99c6de65c6e0c2caec
Piero <pier0ocj77@gmail.com>
2026-06-21 00:23:24 -0500

    feat: mejorar sistema de pedidos con multiples complementos, cambiar el seleccion s/n por cantidades , teniendo en cuneta que cuando pidamos un cafe , no solo lo podemos pedir con doble leche o triple , variara deacuerdo al gusto del cliente , con el anterior main , no tuvimos en cuenta eso

--- src/Runeable/Main.java
+++ src/Runeable/Main.java
@@ -1,10 +1,10 @@
 package Runeable;
 
 import java.util.Scanner;
-
 import Clases.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -16,52 +16,46 @@ public class Main {
         System.out.println("2. Tostado Negro");
         System.out.println("3. Descafeinado");
         System.out.println("4. Batido");
+
         int opcionCafe = Integer.parseInt(sc.nextLine());
 
         System.out.println("Elija el tamaño (N = Normal, M = Mediano, G = Grande):");
         String tamaño = sc.nextLine().toUpperCase();
 
         Cafe bebida;
-        switch (opcionCafe) {
-            case 1:
-                bebida = new Expreso(tamaño);
-                break;
-            case 2:
-                bebida = new TostadoNegro(tamaño);
-                break;
-            case 3:
-                bebida = new Descafeinado(tamaño);
-                break;
-            case 4:
-                bebida = new Batido(tamaño);
-                break;
-            default:
-                bebida = new Expreso(tamaño);
+
+        if (opcionCafe == 1) {
+            bebida = new Expreso(tamaño);
+        } else if (opcionCafe == 2) {
+            bebida = new TostadoNegro(tamaño);
+        } else if (opcionCafe == 3) {
+            bebida = new Descafeinado(tamaño);
+        } else {
+            bebida = new Batido(tamaño);
         }
 
-        String respuesta;
-
-        System.out.println("¿Desea agregar Leche? (S/N)");
-        respuesta = sc.nextLine();
-        if (respuesta.equalsIgnoreCase("S")) {
+        System.out.println("Cuantas Leches desea? (0,1,2...)");
+        int leche = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < leche; i++) {
             bebida = new leche(bebida);
         }
 
-        System.out.println("¿Desea agregar Moca? (S/N)");
-        respuesta = sc.nextLine();
-        if (respuesta.equalsIgnoreCase("S")) {
+        System.out.println("Cuantas Mocas desea? (0,1,2...)");
+        int moca = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < moca; i++) {
             bebida = new Moca(bebida);
         }
 
-        System.out.println("¿Desea agregar Soya? (S/N)");
-        respuesta = sc.nextLine();
-        if (respuesta.equalsIgnoreCase("S")) {
+        System.out.println("Cuantas Soyas desea? (0,1,2...)");
+        int soya = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < soya; i++) {
             bebida = new Soya(bebida);
         }
 
-        System.out.println("¿Desea agregar Crema? (S/N)");
-        respuesta = sc.nextLine();
-        if (respuesta.equalsIgnoreCase("S")) {
+
+        System.out.println("Cuantas Cremas desea? (0,1,2...)");
+        int crema = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < crema; i++) {
             bebida = new Crema(bebida);
         }
 
